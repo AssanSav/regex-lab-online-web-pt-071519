d4b0598f51bf0b215180be8b33a157490a22c8b1
assane savadogo <assane.savadogo81@gmail.com>
2019-09-16 22:11:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+  word.match(/^[aiuoeAUIOE]+\w/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  #text = text.split(" ")
+  #text.grep(/^\w{5}\b/)
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].+\W$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/\d/).count == 10
+  #phone.scan(/\W^[0-9]{10}/) ? true : false
 end
